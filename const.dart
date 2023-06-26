1d370c2b30f444e706bc79293dcee95e0037b270
Andriyko123SSS <137785089+Andriyko123SSS@users.noreply.github.com>
2023-06-26 14:26:21 +0300

    Create const.dart
    My constants

---
+++ const.dart
@@ -0,0 +1,5 @@
+const String apiBaseUrl = 'https://api.example.com';
+const int timeoutDuration = 5000;
+final List<String> supportedLanguages = ['en', 'fr', 'es'];
+const int scammedtimes = 100;
+const name = 'fred';
