7946a6fc805ba6be15d5b284ba15aaa5ece9e5bf
Andriyko123SSS <137785089+Andriyko123SSS@users.noreply.github.com>
2023-08-02 16:15:06 +0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -7,13 +7,6 @@ d
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Hello World App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-         primarySwatch: Colors.blue,idget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Hello World App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
          primarySwatch: Colors.blue,
