45482ccd2408efa0f10e33f399d885fb112e9887
Andriyko123SSS <137785089+Andriyko123SSS@users.noreply.github.com>
2023-08-13 18:20:06 +0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -14,10 +14,6 @@ class MyApp extends StatelessWidget {
             style: TextStyle(fontSize: 24),
           ),
         ),
-      ),
-    );
-  }
-}
 class MyApp extends StatelessWidget {
     );home: Scaffold(
         Arrested capital))))
