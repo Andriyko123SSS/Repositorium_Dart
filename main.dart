121cb00bcd39db73ece1ad12741c8813c7217050
Andriyko123SSS <137785089+Andriyko123SSS@users.noreply.github.com>
2023-08-14 15:48:26 +0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -4,14 +4,6 @@ void main() {
   runApp(MyApp());
 }
 d
-class MyApp extends StatelessWidget {
-    );home: Scaffold(
-        Arrested capital))))
-        ),
-        body: Center(
-          child: Text(
-            'Hello, World! ++',
-            style: TextStyle(fontSize: 24),
           ),
         ),
 class MyApp extends StatelessWidget {
