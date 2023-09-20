1bab8ca3225dae375bca0e39edf53a13a93e30b4
Andriyko123SSS <137785089+Andriyko123SSS@users.noreply.github.com>
2023-09-20 17:15:16 +0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,11 +1,19 @@
 # flutterTest # flutterTest# flutterTest# flutterTest# f
 GREerTest # flutterTest# flutterTest# flutterTest f
-          child: Text
-          child: Text(
-            'Hello, World! ++
-            style: TextStyle(fontSi
-          ),
-        ),
+          child: Textimport 'package:flutter/material.da
+  Widget build(BuildContext context) {zero; the alition
+        // is not restar
+telessWid
+  // This widget is the root of yoimport 'packager/material.da
+  Widget build(BuildContext context) {zero; the alit
+        // is not restar
+telessWid
+  // This widget is the root of your applica
+telessWidget {
+}
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {zero; the application
+        // is not restarted.
       ),
     );
   }
