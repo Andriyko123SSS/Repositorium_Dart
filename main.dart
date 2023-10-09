6e20e641102040114a172b592dd463d255b746cf
Andriyko123SSS <137785089+Andriyko123SSS@users.noreply.github.com>
2023-10-09 23:12:21 +0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -4,10 +4,6 @@ GREerTest # flutterTest# flutterTest# flutterTest f
   Widget build(BuildContext context) {zeroackager/material
   Widget build(BuildContext context) {zero; the alit
 telessWid
-  // This widget is the root of your applica
-    );
-  }
-class MyApp extends StatelessWidget {
                     await page4.waiorNetworkIdle(l;
                     await page4.click(elementSeIdl;
                     await page4.waitForSelector(elementSelector4);
