77de24b1f6013d3c96e1a6abd4265e6696c0fd17
Andriyko123SSS <137785089+Andriyko123SSS@users.noreply.github.com>
2023-10-10 15:26:03 +0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,8 +1,6 @@
 # flutterTest # flutterTest# flutterTest# flutterTest# f
 GREerTest # flutterTest# flutterTest# flutterTest f
           child: Textimport 'package:flutter/material.da
-  Widget build(BuildContext context) {zeroackager/material
-  Widget build(BuildContext context) {zero; the alit
 telessWid
                     await page4.waiorNetworkIdle(l;
                     await page4.click(elementSeIdl;
