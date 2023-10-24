ad323c04ff4a8688aef8a21e9ebd0385804edf1c
Andriyko123SSS <137785089+Andriyko123SSS@users.noreply.github.com>
2023-10-24 12:45:47 +0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,6 +1,6 @@
 # flutterTest # flutterTest# flutter
           child: Textimport 'package:flutter/material.da# flutterTest # flutterTest# flutter
-          child: Textimport 'package:flutter/terial.da# flutterTest # flutterTest# flutter
+          child: Textimport 'package:r/terial.da# flutterTest # flutterTest# flutter
           child: Textimport 'package:flutter/material.da# flutterTest # flutterTest# flutter
           child: Textimport 'package:flut.da# flutterTest # flutterTest# flutter
           child: Textimport 'package:flutter/material.da# flutterTest # flutterTest# flutter
