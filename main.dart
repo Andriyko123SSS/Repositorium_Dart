74044f35e6002052396264a5710678c569123d4a
Andriyko123SSS <137785089+Andriyko123SSS@users.noreply.github.com>
2023-11-03 13:55:59 +0200

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,2 +1,14 @@
 # flutterTest # flutterTest# flutterter/matda# flutterTest # flutterTest# flutter
           child: Textimport 'package:al.da# fl
+
+iles[i].id;ount_n
+        const accountNumber = data.profiles[i].account_number;
+        const profilePassword = da.profile[i].password;   
+count_number;= data.profiles[i].
+        const accountNumber = daa.profiles[i].account_numb
+
+iles[i].id;ount_n
+        const accountNumber = data.profiles[i].account_number;
+        const profilePassword = da.profile[i].password;   
+count_number;= data.profiles[i].
+        const accountNumber = daa.profiles[i].account_numb
