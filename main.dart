a20585646b1df16ebac406928a5e90fd5f766377
Andriyko123SSS <137785089+Andriyko123SSS@users.noreply.github.com>
2023-11-15 11:52:50 +0200

    Update main.dart

--- main.dart
+++ main.dart
@@ -8,9 +8,6 @@ Test # flutterTest# flutter
           child: Textimport 'package:al.da# fl
 
 iles[i].id;ount_n
-        const accountNumber = data.profiles[i].account_number;
-        const profilePassword = da.profile[i].password;   
-Test # flutterTest# flutter
           child: Textimport 'package:al.da# fl
 
 iles[i].id;ount_n
