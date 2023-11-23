0b5b9ef6ba252f0215de4d06fa243bbb709c6872
Andriyko123SSS <137785089+Andriyko123SSS@users.noreply.github.com>
2023-11-23 18:33:53 +0200

    Update main.dart

--- main.dart
+++ main.dart
@@ -4,11 +4,7 @@
 iles[i].id;ount_n
           child: Textimport 'package:al.da# fl
 # flutterTest # flutterTest# flutterter/matda# flutterTest # flutterTest
-iles[i].id;ount_n
-          child: Textimport 'package:al.da# fl
-
-iles[i].id;ount_n
-iles[i].id;ount_n# flutterTest # flutterTest# flutterter/matda# flutterTest # flutterTest# flutter
+iles[i].id;ount_n/matda# flutterTest # flutterTest# flutter
           child: Textimport 'package:al.da# fl
 
 iles[i].id;ount_n
