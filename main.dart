d3870e97dd84128472f1e6947ab8c92421c06fc1
Andriyko123SSS <137785089+Andriyko123SSS@users.noreply.github.com>
2024-01-31 14:07:06 +0200

    Update main.dart

--- main.dart
+++ main.dart
@@ -5,14 +5,8 @@ s const
 = daa.nst e(
         const accountNumber = data.profiles[i]
 umber;.profile[i].passwors[i]s const 
-umber;.profile[i].passwors[i]s const 
-є
-ghgfgf
-s const 
-= daa.nst e(
-        const accountNumber = data.profiles[i]
-umber;.profile[i].passwors[i]s const 
-= daa.nst 
+umber;.profile[i].passwors[i]s const uytj=llllllllllluytj=llllllllllluytj=llllllllllluytj=llllllllllluytj=llllllllllllllluytj=llllllllllllllllll
+lllluytj=llllllllllluytj=llllllllllluytj=llllllllllluytj=llllllllllluytj=llllllllllluytj=llllllllllluytj=llllllllllluytj=llllllllllluytj=llllllllllluytj=llllllllll
 umber;.profile[i].passwors[i]s const 
 
 umber;.profile[i].passwors[i]s const 
